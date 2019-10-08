922a327dab65b05f8c7a95f70bb7393911dab813
Maaruf Dauda <mkdauda.work@gmail.com>
2019-10-08 15:01:33 +0100

    :sparkles: Set up simple reducer and initial list

---
+++ todo/src/reducer/reducer.js
@@ -0,0 +1,40 @@
+import { useReducer } from "react";
+
+const ON_ADD_TODO = "ON_ADD_TODO";
+const ON_MARK_COMPLETE = "ON_MARK_COMPLETE";
+
+export const initialList = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: "Learn about functions",
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: "Learn about state",
+    completed: false,
+    id: 3892987589
+  },
+  {
+    item: "Learn about context",
+    completed: false,
+    id: 3892987589
+  }
+];
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ON_ADD_TODO: {
+      return [];
+    }
+    case ON_MARK_COMPLETE: {
+      return [];
+    }
+    default:
+      return state;
+  }
+};
